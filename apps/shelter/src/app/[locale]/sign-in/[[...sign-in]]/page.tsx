56e36c54ea1d528ae6712fb29e4ee4d826499042
Victoria <cabezosvic@gmail.com>
2026-08-07 13:19:29 +0200

    Clerk theming: single static appearance via CSS variables (#151)

--- apps/shelter/src/app/[locale]/sign-in/[[...sign-in]]/page.tsx
+++ apps/shelter/src/app/[locale]/sign-in/[[...sign-in]]/page.tsx
@@ -1,13 +1,9 @@
-"use client";
-
 import { SignIn } from "@clerk/nextjs";
-import { useClerkAppearance } from "@/components/clerk-appearance";
 
 export default function SignInPage() {
-  const appearance = useClerkAppearance();
   return (
     <div className="flex flex-1 items-center justify-center">
-      <SignIn appearance={appearance} fallbackRedirectUrl="/organizations" />
+      <SignIn fallbackRedirectUrl="/organizations" />
     </div>
   );
 }
--- apps/shelter/src/app/[locale]/sign-up/[[...sign-up]]/page.tsx
+++ apps/shelter/src/app/[locale]/sign-up/[[...sign-up]]/page.tsx
@@ -1,13 +1,9 @@
-"use client";
-
 import { SignUp } from "@clerk/nextjs";
-import { useClerkAppearance } from "@/components/clerk-appearance";
 
 export default function SignUpPage() {
-  const appearance = useClerkAppearance();
   return (
     <div className="flex flex-1 items-center justify-center">
-      <SignUp appearance={appearance} fallbackRedirectUrl="/organizations" />
+      <SignUp fallbackRedirectUrl="/organizations" />
     </div>
   );
 }
